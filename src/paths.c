5ed1fe917e4c89cd3bfc4c9219cdb99249e701c6
programming101dev <113087924+programming101dev@users.noreply.github.com>
2026-08-06 03:18:08 -0700

    Enforce semantic source contracts

--- src/paths.c
+++ src/paths.c
@@ -4,7 +4,16 @@
 #include <errno.h>
 #include <p101_c/p101_stdio.h>
 #include <p101_c/p101_string.h>
-#include <p101_process/process.h>
+#include <p101_process/p101_sched.h>
+#include <p101_process/p101_setjmp.h>
+#include <p101_process/p101_signal.h>
+#include <p101_process/p101_spawn.h>
+#include <p101_process/p101_stdio.h>
+#include <p101_process/p101_stdlib.h>
+#include <p101_process/p101_unistd.h>
+#include <p101_process/sys/p101_resource.h>
+#include <p101_process/sys/p101_times.h>
+#include <p101_process/sys/p101_wait.h>
 #include <stdio.h>
 #include <string.h>
 
--- src/runner.c
+++ src/runner.c
@@ -7,7 +7,16 @@
 #include "result.h"
 #include <p101_c/p101_stdio.h>
 #include <p101_c/p101_string.h>
-#include <p101_process/process.h>
+#include <p101_process/p101_sched.h>
+#include <p101_process/p101_setjmp.h>
+#include <p101_process/p101_signal.h>
+#include <p101_process/p101_spawn.h>
+#include <p101_process/p101_stdio.h>
+#include <p101_process/p101_stdlib.h>
+#include <p101_process/p101_unistd.h>
+#include <p101_process/sys/p101_resource.h>
+#include <p101_process/sys/p101_times.h>
+#include <p101_process/sys/p101_wait.h>
 #include <p101_util/tool_run.h>
 #include <stdio.h>
 #include <stdlib.h>
